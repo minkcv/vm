4185ede8a736e82feaa98591e99467094dee2111
Will Smith <minkcv@gmail.com>
2017-12-20 10:50:54 -0700

    add bash script to run tests

---
+++ compiler/runtests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+i=1;
+while [ -f "./tests/test$i.src" ]; do
+    echo "Compiling test $i"
+    ./compiler -f tests/test$i.src
+    echo "Test $i source:"
+    cat ./tests/test$i.src
+    echo "Test $i assembly:"
+    cat ./tests/test$i.asm
+    i=$((i+1))
+done
