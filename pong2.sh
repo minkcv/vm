e9255bbf1938878aab1b22edfd62fa9778c77d81
Will Smith <minkcv@gmail.com>
2017-12-25 19:57:17 -0700

    fix pong2 script

--- pong2.sh
+++ pong2.sh
@@ -3,7 +3,7 @@ cd compiler
 make &> /dev/null
 cd ..
 make &> /dev/null
-./compiler/compiler -f pong2/pong2.src &> /dev/null
+./compiler/compiler -f pong2/pong2.vapor &> /dev/null
 ./assembler -f pong2/pong2.asm &> /dev/null
 ./main -f pong2/pong2.bin -s 2 &> /dev/null
 
