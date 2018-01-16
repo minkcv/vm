8ebfefc1750b82a597665b86f04ffd2a0b6392b8
Will Smith <minkcv@gmail.com>
2018-01-15 18:12:28 -0700

    add shapes and symbol sprites

--- games/paint.sh
+++ games/paint.sh
@@ -10,8 +10,12 @@ make &> /dev/null
 ./compiler/compiler -f games/paint/paint.vapor &> /dev/null
 ./convertsprite games/paint/abc.png &> /dev/null
 ./convertsprite games/paint/123.png &> /dev/null
+./convertsprite games/paint/sym.png &> /dev/null
+./convertsprite games/paint/shapes.png &> /dev/null
 cat games/paint/abc.spr >> games/paint/paint.rom
 cat games/paint/123.spr >> games/paint/paint.rom
+cat games/paint/sym.spr >> games/paint/paint.rom
+cat games/paint/shapes.spr >> games/paint/paint.rom
 ./assembler -f games/paint/paint.asm &> /dev/null
 ./main -f games/paint/paint.bin -r games/paint/paint.rom -s 2 &> /dev/null
 
