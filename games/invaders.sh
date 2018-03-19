2b81f6d44652984bc5b610adb2bb320efe3b72ed
Will Smith <minkcv@gmail.com>
2018-03-19 15:16:23 -0600

    invaders game started

---
+++ games/invaders.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm invaders/invaders.asm
+rm invaders/invaders.bin
+rm invaders/invaders.rom
+cd ../compiler
+make
+cd ..
+make
+cd games/invaders
+../../convertsprite player.png
+../../convertsprite bullet.png
+cat player.spr >> invaders.rom
+cat bullet.spr >> invaders.rom
+cd ../..
+./compiler/compiler -f games/invaders/invaders.vapor -l sa
+./assembler -f games/invaders/invaders.asm
+./main -f games/invaders/invaders.bin -r games/invaders/invaders.rom -d
+
