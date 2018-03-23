147f3c9610b76a5c19a7a32adc6b68e85aaf1fe7
Will Smith <minkcv@gmail.com>
2018-03-23 12:43:11 -0600

    add enemies

--- games/invaders.sh
+++ games/invaders.sh
@@ -9,8 +9,10 @@ make
 cd games/invaders
 ../../convertsprite player.png
 ../../convertsprite bullet.png
+../../convertsprite enemy.png
 cat player.spr >> invaders.rom
 cat bullet.spr >> invaders.rom
+cat enemy.spr >> invaders.rom
 cd ../..
 ./compiler/compiler -f games/invaders/invaders.vapor -l sa
 ./assembler -f games/invaders/invaders.asm
