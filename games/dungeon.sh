9b3ea7a3535db38ac9fb63baf2906d05d18f669c
Will Smith <minkcv@gmail.com>
2018-12-17 13:51:46 -0700

    all non door related walls showing

--- games/dungeon.sh
+++ games/dungeon.sh
@@ -7,6 +7,10 @@ make &> /dev/null
 cd ..
 make &> /dev/null
 ./convertsprite games/dungeon/wall_corner.png
+./convertsprite games/dungeon/wall_horizontal.png
+./convertsprite games/dungeon/wall_vertical.png
 cat games/dungeon/wall_corner.spr >> games/dungeon/dungeon.rom
+cat games/dungeon/wall_horizontal.spr >> games/dungeon/dungeon.rom
+cat games/dungeon/wall_vertical.spr >> games/dungeon/dungeon.rom
 ./compiler/compiler -f games/dungeon/dungeon.vapor -l s
 ./assembler -f games/dungeon/dungeon.asm
