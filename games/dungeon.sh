d6263eb18fcb2f9c14c139f893bff0b3c5cb9629
Will Smith <minkcv@gmail.com>
2018-12-17 13:16:58 -0700

    corner wall sprites working

--- games/dungeon.sh
+++ games/dungeon.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 rm dungeon/dungeon.bin &> /dev/null
 rm dungeon/dungeon.asm &> /dev/null
+rm dungeon/dungeon.rom &> /dev/null
 cd ../compiler
 make &> /dev/null
 cd ..
 make &> /dev/null
+./convertsprite games/dungeon/wall_corner.png
+cat games/dungeon/wall_corner.spr >> games/dungeon/dungeon.rom
 ./compiler/compiler -f games/dungeon/dungeon.vapor -l s
 ./assembler -f games/dungeon/dungeon.asm
