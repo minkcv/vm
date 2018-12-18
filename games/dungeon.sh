704a989a5775315081dcde61a217b13b3cba5239
Will Smith <minkcv@gmail.com>
2018-12-18 12:31:29 -0700

    add door sprites, start on map data

--- games/dungeon.sh
+++ games/dungeon.sh
@@ -9,8 +9,16 @@ make &> /dev/null
 ./convertsprite games/dungeon/wall_corner.png
 ./convertsprite games/dungeon/wall_horizontal.png
 ./convertsprite games/dungeon/wall_vertical.png
+./convertsprite games/dungeon/door_horizontal.png
+./convertsprite games/dungeon/door_vertical.png
 cat games/dungeon/wall_corner.spr >> games/dungeon/dungeon.rom
 cat games/dungeon/wall_horizontal.spr >> games/dungeon/dungeon.rom
 cat games/dungeon/wall_vertical.spr >> games/dungeon/dungeon.rom
+cat games/dungeon/door_horizontal.spr >> games/dungeon/dungeon.rom
+cat games/dungeon/door_vertical.spr >> games/dungeon/dungeon.rom
+cd games/dungeon
+python map.py
+cd ../..
+cat games/dungeon/mapdata >> games/dungeon/dungeon.rom
 ./compiler/compiler -f games/dungeon/dungeon.vapor -l s
 ./assembler -f games/dungeon/dungeon.asm
