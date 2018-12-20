23d28e6d4b608fbe3508e1fce088868782b68e31
Will Smith <minkcv@gmail.com>
2018-12-20 13:49:41 -0700

    change room tile format, increase png size for room tiles

--- games/dungeon.sh
+++ games/dungeon.sh
@@ -14,10 +14,10 @@ make &> /dev/null
 ./convertsprite games/dungeon/door_horizontal.png # 128 bytes
 ./convertsprite games/dungeon/door_vertical.png # 128 bytes
 # 768 bytes of rom used
-./convertsprite games/dungeon/tiles.png # 2048 bytes
-# 2816 bytes of rom used
+./convertsprite games/dungeon/tiles.png # 16384 bytes
+# 17152 bytes of rom used
 ./convertsprite games/dungeon/maptiles.png # 256 bytes
-# 3072 bytes of rom used
+# 17408 bytes of rom used
 cat games/dungeon/wall_corner.spr >> games/dungeon/dungeon.rom
 cat games/dungeon/wall_horizontal.spr >> games/dungeon/dungeon.rom
 cat games/dungeon/wall_vertical.spr >> games/dungeon/dungeon.rom
@@ -27,7 +27,7 @@ cat games/dungeon/tiles.spr >> games/dungeon/dungeon.rom
 cat games/dungeon/maptiles.spr >> games/dungeon/dungeon.rom
 cd games/dungeon
 python map.py # 2304 bytes
-# 5376 bytes of rom used
+# 19712 bytes of rom used
 cd ../..
 cat games/dungeon/mapdata >> games/dungeon/dungeon.rom
 ./compiler/compiler -f games/dungeon/dungeon.vapor -l sa
