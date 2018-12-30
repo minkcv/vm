28b7f1891e5366184821c74c482da56cf762016d
Will Smith <minkcv@gmail.com>
2018-12-30 15:02:21 -0700

    make script run game

--- games/fallen.sh
+++ games/fallen.sh
@@ -6,17 +6,17 @@ cd ../compiler
 make &> /dev/null
 cd ..
 make &> /dev/null
-./convertsprite games/fallen/wall_corner.png # 256 bytes
+./convertsprite games/fallen/wall_corner.png &> /dev/null # 256 bytes
 # 256 bytes of rom used
-./convertsprite games/fallen/wall_horizontal.png # 128 bytes
-./convertsprite games/fallen/wall_vertical.png # 128 bytes
+./convertsprite games/fallen/wall_horizontal.png &> /dev/null # 128 bytes
+./convertsprite games/fallen/wall_vertical.png &> /dev/null # 128 bytes
 # 512 bytes of rom used
-./convertsprite games/fallen/door_horizontal.png # 128 bytes
-./convertsprite games/fallen/door_vertical.png # 128 bytes
+./convertsprite games/fallen/door_horizontal.png &> /dev/null # 128 bytes
+./convertsprite games/fallen/door_vertical.png &> /dev/null # 128 bytes
 # 768 bytes of rom used
-./convertsprite games/fallen/tiles.png # 16384 bytes
+./convertsprite games/fallen/tiles.png &> /dev/null # 16384 bytes
 # 17152 bytes of rom used
-./convertsprite games/fallen/maptiles.png # 256 bytes
+./convertsprite games/fallen/maptiles.png &> /dev/null # 256 bytes
 # 17408 bytes of rom used
 cat games/fallen/wall_corner.spr >> games/fallen/fallen.rom
 cat games/fallen/wall_horizontal.spr >> games/fallen/fallen.rom
@@ -26,12 +26,14 @@ cat games/fallen/door_vertical.spr >> games/fallen/fallen.rom
 cat games/fallen/tiles.spr >> games/fallen/fallen.rom
 cat games/fallen/maptiles.spr >> games/fallen/fallen.rom
 cd games/fallen
-python map.py # 2304 bytes
+python map.py &> /dev/null # 2304 bytes
 cd ../..
 cat games/fallen/mapdata >> games/fallen/fallen.rom
 # 19712 bytes of rom used, start of segment 77 from rom start . (segment 205 from 0)
-./convertsprite games/fallen/player.png # 64 bytes
+./convertsprite games/fallen/player.png &> /dev/null # 64 bytes
 cat games/fallen/player.spr >> games/fallen/fallen.rom
 # 19778 bytes used
-./compiler/compiler -f games/fallen/fallen.vapor -l sa
+./compiler/compiler -f games/fallen/fallen.vapor &> /dev/null
 ./assembler -f games/fallen/fallen.asm
+./main -f games/fallen/fallen.bin -r games/fallen/fallen.rom -s 2 &> /dev/null
+
